379e9ff750f4c18d92a0f36388dce4619c7a442a
HyeongMinKim <34018441+FDLKHM@users.noreply.github.com>
2021-02-09 22:17:06 +0900

    Add files via upload

---
+++ chat/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"trace"
+)
+
+//tmpl은 하나의 템플릿을 나타냄
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP가 HTTP 요청을 처리한다
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // 플래그 파싱
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// 방을 가져옴
+
+	go r.run()
+
+	// 웹 서버 시작
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
